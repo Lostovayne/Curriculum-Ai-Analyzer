4c7aa1a70a9421b2e8a8ae8b1beb6e9b431bc62e
LostoVayne <moises.barrera.dev@gmail.com>
2025-07-23 23:01:09 -0400

    Add auth function

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
 import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
 
@@ -11,6 +14,13 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover md:mx-2.5 md:mt-2.5">
       <Navbar />
